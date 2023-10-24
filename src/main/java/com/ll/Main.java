7b40f010a3af18d00c9a135615736b997c50211e
i-am-jiwon <dlark1128@gmail.com>
2023-10-24 17:21:36 +0900

    qutoes step6 & 7

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -11,11 +11,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
-
         order order1 = new order();
-
         order1.orderRun();
-
     }
 }
 
@@ -30,11 +27,16 @@ class order{
     ArrayList<String> words = new ArrayList<String>();
 
 
+    String delete;
+    String id;
+    int indexId;
+
     void orderRun() {
         while(cond == 0) {
             System.out.print("명령) ");
             Scanner sc = new Scanner(System.in);
             String order = sc.nextLine();
+            delete = order.substring(0,2);
             if (order.equals("종료")) {
                 cond = 1;
             }
@@ -53,9 +55,21 @@ class order{
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
                 for(int i=listNum.size()-1; i>=0 ; i--){
-                    System.out.println(listNum.get(i) +" / "+ names.get(i)+" / "+words.get(i));
+                    if(listNum.get(i) != 0)
+                        System.out.println(listNum.get(i) +" / "+ names.get(i)+" / "+words.get(i));
                 }
             }
+            if (delete.equals("삭제")){
+                id = order.substring(6);
+                indexId = Integer.parseInt(id);
+                if(listNum.get(indexId-1) == 0) {
+                    System.out.println(id + "번 명언은 존재하지 않습니다.");
+                }else {
+                    listNum.set(indexId - 1, 0);
+                    System.out.println(id + "번 명언이 삭제되었습니다.");
+                }
+            }
+
         }
     }
 }
