d493290f6d2555652596d370c1970242b42be31b
i-am-jiwon <dlark1128@gmail.com>
2023-10-24 14:28:36 +0900

    qutoes step2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,16 +1,36 @@
 package com.ll;
 
+
 import java.util.Scanner;
+
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        Scanner sc = new Scanner(System.in);
-        String order = sc.next();
 
-        if(order.equals("종료")){
-            return;
+        order order1 = new order();
+
+        order1.orderRun();
+
+    }
+}
+
+class order{
+    int cond = 0;
+    void orderRun() {
+        while(cond == 0) {
+            System.out.print("명령) ");
+            Scanner sc = new Scanner(System.in);
+            String order = sc.nextLine();
+            if (order.equals("종료")) {
+                cond = 1;
+            } else if (order.equals("등록")) {
+                System.out.print("명언 : ");
+                String order2 = sc.nextLine();
+                System.out.print("작가 : ");
+                String order3 = sc.nextLine();
+            }
         }
     }
 }
