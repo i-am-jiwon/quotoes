1ccead953f6509a8df22b555fca92bbb56448ad1
i-am-jiwon <dlark1128@gmail.com>
2023-10-24 13:47:38 +0900

    qutoes step1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,16 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        Scanner sc = new Scanner(System.in);
+        String order = sc.next();
+
+        if(order.equals("종료")){
+            return;
+        }
     }
 }
